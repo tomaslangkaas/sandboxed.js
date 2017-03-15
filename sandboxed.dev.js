1d03c239e57abd34c54d724115ec73912a939ab1
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2017-03-15 21:26:40 +0100

    Update sandboxed.dev.js

--- sandboxed.dev.js
+++ sandboxed.dev.js
@@ -1,24 +1,45 @@
-var sandboxed = (function(){
-	var expRe=/(>>\=|<<\=|\+\+|\-\-)|\d*\.?\d+(?:[eE][+\-]?\d+)?|[\!\=<>]\=\=?|"(?:\\"|\\\\|\\n|[^\\"])*"|(\w+)(\()?|([^<>%\(\)\/\+\-\*\&\|\^\~\:\,\?;])/g;
+var sandboxed = (function() {
+  var expRe =
+    /(>>\=|<<\=|\+\+|\-\-)|\d*\.?\d+(?:[eE][+\-]?\d+)?|[\!\=<>]\=\=?|"(?:\\"|\\\\|\\n|[^\\"])*"|(\w+)(\()?|(\(|\))|([^<>%\/\+\-\*\&\|\^\~\:\,\?\!;])/g;
 
-	var expReFn=function(m, s, i, p, n){
-		return (n?'':(s?'':(i?(p?'f.hasOwnProperty("'+i+'")&&f["'+i+'"](':'d["'+i+'"]'):m)));
-	};
+  var expReFn = function(match, blacklist, identifier,
+    functionCall, group, nonWhitelist) {
+    return (
+      nonWhitelist ? 
+        '' : 
+        (blacklist ? 
+          '' : 
+          (identifier ?
+            (functionCall ? 
+              '(f.hasOwnProperty("' + identifier +
+              '")&&f["' + identifier + '"](' :
+              '(d.hasOwnProperty("' + identifier + 
+              '")&&d["' + identifier + '"])'
+            ) : 
+            (group ? 
+              group + group :
+              match
+            )
+          )
+        )
+      );
+  };
 
-	function compileExpression(jsExpression){
-		return jsExpression.replace(expRe,expReFn);
-	}
+  function compileExpression(jsExpression) {
+    return jsExpression.replace(expRe, expReFn);
+  }
 
-	function buildFunction(jsExpression){
-		try{
-			//data object, functions object, error handler
-			return new Function('d','f','e','try{'+compileExpression(jsExpression)+'}catch(r){e&&e(r)}');
-		}catch(e){
-			return false;
-		}
-	}
+  function buildFunction(jsExpression, returnOnFailure) {
+    try {
+      //data object, functions object, error handler
+      return new Function('d', 'f', 'e', 'try{return ' +
+        compileExpression(jsExpression) + '}catch(r){e&&e(r)}');
+    } catch(e) {
+      return returnOnFailure || false;
+    }
+  }
 
-	buildFunction['compile'] = compileExpression;
+  buildFunction['compile'] = compileExpression;
 
-	return buildFunction;
+  return buildFunction;
 })();
